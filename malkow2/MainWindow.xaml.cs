0e12d80f5138118024205b15b6cc69627d598c0d
ooagu <ooagu@DESKTOP-TII5S3M>
2024-12-02 15:30:36 +0500

    Добавьте файлы проекта.

---
+++ malkow2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace malkow2
+{
+    public partial class MainWindow : Window
+    {
+        // коллекция хранения
+        public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // text
+            Products.Add(new Product("Ноутбук", 50000m, "Игровой ноутбук, хороший процессор, не греется"));
+            Products.Add(new Product("Смартфон", 30000m, "Смартфон, крепкий корпус не разобьешь, пока не захочешь"));
+            Products.Add(new Product("Наушники", 5000m, "Беспроводные, в трех цветах на выбор"));
+
+            ProductList.ItemsSource = Products;
+        }
+
+        // выбор товара
+        private void ProductListSelectionChange(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (ProductList.SelectedItem is Product selectedProduct)
+            {
+                // деконстр извлеч
+                var (name, price, description) = selectedProduct;
+
+                // отображение
+                ProductName.Text = $"название: {name}";
+                ProductPrice.Text = $"цена: {price:C}";
+                ProductDescription.Text = $"описание: {description}";
+            }
+
+        }
+    }
+}
---
+++ malkow2/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace malkow2
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        // конструктор
+        public Product(string name, decimal price, string description)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+        }
+
+        // деконструктор
+        public void Deconstruct(out string name, out decimal price, out string description)
+        {
+            name = Name;
+            price = Price;
+            description = Description;
+        }
+
+    }
+}
